b8728caecf79f1dc1b1fb6907ac76dd209f2517f
naazy <marziyeh.naaz.ahmed@gmail.com>
2017-01-20 14:51:30 +0000

    adding better test names for dateUtilsTest as suggested by @roryc89

--- tests/DateUtilsTests.elm
+++ tests/DateUtilsTests.elm
@@ -6,41 +6,40 @@ import DateUtils
 import Date
 
 
-all: Test
+all : Test
 all =
+    -- 1497744000 is 18 june 2017 00:00 gmt
     describe "Date Utils"
-        [
-        test "parse default" <|
-                \() ->
-                    Expect.equal (DateUtils.parse "not Date") (Date.fromTime 0)
-        , test "parse" <|
-                \() ->
-                    Expect.equal (DateUtils.parse "1970-01-01T00:00:00.100+00:00") (Date.fromTime 100)
-        , test "dateWithoutTimeToDate" <|
-                \() ->
-                    Expect.equal (DateUtils.dateWithoutTimeToDate { year = 1970, month = 1, day = 1 }) (Date.fromTime 0)
-        , test "dateToDateWithoutTime" <|
-                \() ->
-                    Expect.equal (DateUtils.dateToDateWithoutTime (Date.fromTime 0)) ({ year = 1970, month = 1, day = 1 })
-
-        , test "timeOfDayToTime"  <|
-              \() ->
-                    Expect.equal (DateUtils.timeOfDayToTime { year = 1970, month = 1, day = 1 } { hour = 0, minute = 0 }) 0
-
-        , test "dateWithoutTimeToValueString"  <|
-                \() ->
-                      Expect.equal (DateUtils.dateWithoutTimeToValueString { year = 1970, month = 1, day = 1 }) "1970-1-1"
-        , test "valueStringToDateWithoutTime"  <|
-                \() ->
-                      Expect.equal (DateUtils.valueStringToDateWithoutTime "1970-1-1") { year = 1970, month = 1, day = 1 }
-        , test "displayDateWithoutTime"  <|
-                \() ->
-                      Expect.equal (DateUtils.displayDateWithoutTime { year = 1970, month = 1, day = 1 }) "1 Jan"
-        , test "displayTimeOfDay"  <|
-                \() ->
-                      Expect.equal (DateUtils.displayTimeOfDay { hour = 0, minute = 0 }) "00:00"
-        , test "getDateMonthInt"  <|
-                \() ->
-                  -- 2764800000 should be february
-                      Expect.equal (DateUtils.getDateMonthInt (Date.fromTime 2764800000)) 2
+        [ test "parse function defaults to 0 if it is passed an invalid date" <|
+            \() ->
+                Expect.equal (DateUtils.parse "not Date") (Date.fromTime 0)
+        , test "parse appears to correctly parse dates" <|
+            \() ->
+                Expect.equal
+                    (DateUtils.parse "2017-06-18T00:00:00.000+00:00")
+                    (Date.fromTime 1497744000000)
+        , test "dateWithoutTimeToDate correctly converts a date record without a time to the correct date" <|
+            \() ->
+                Expect.equal (DateUtils.dateWithoutTimeToDate { year = 2017, month = 6, day = 18 }) (Date.fromTime 1497740400000)
+        , test "dateToDateWithoutTime correctly converts a date to our date record without a time" <|
+            \() ->
+                Expect.equal (DateUtils.dateToDateWithoutTime (Date.fromTime 1497740400000)) ({ year = 2017, month = 6, day = 18 })
+        , test "timeOfDayToTime correct converts our time our time of day record to the correct time (ms)" <|
+            \() ->
+                Expect.equal (DateUtils.timeOfDayToTime { year = 2017, month = 6, day = 18 } { hour = 0, minute = 0 }) 1497740400000
+        , test "dateWithoutTimeToValueString correctly changes format of dateWithoutTime record to readable numerical string" <|
+            \() ->
+                Expect.equal (DateUtils.dateWithoutTimeToValueString { year = 2017, month = 6, day = 18 }) "2017-6-18"
+        , test "valueStringToDateWithoutTime correct converts readable date string to dateWithoutTime record" <|
+            \() ->
+                Expect.equal (DateUtils.valueStringToDateWithoutTime "2017-6-18") { year = 2017, month = 6, day = 18 }
+        , test "displayDateWithoutTime converts dateWithoutTime records to readable date format with 3 letter word for the month" <|
+            \() ->
+                Expect.equal (DateUtils.displayDateWithoutTime { year = 2017, month = 6, day = 18 }) "18 Jun"
+        , test "displayTimeOfDay correctly converts our time record to a readable string" <|
+            \() ->
+                Expect.equal (DateUtils.displayTimeOfDay { hour = 9, minute = 32 }) "09:32"
+        , test "getDateMonthInt correctly gets the numerical month from the date" <|
+            \() ->
+                Expect.equal (DateUtils.getDateMonthInt (Date.fromTime 1497744000000)) 6
         ]
