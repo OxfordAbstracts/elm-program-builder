681ceef179825d03db21ea79553395a3362d663c
naazy <marziyeh.naaz.ahmed@gmail.com>
2017-01-20 11:38:18 +0000

    circle ci is clean

--- circle-deps.sh
+++ circle-deps.sh
@@ -1,7 +1,7 @@
 # Installs sysconfcpus so we can make elm compile faster
 # and limit the CPUS that elm uses to compile.
 # See: https://github.com/elm-lang/elm-compiler/issues/1473#issuecomment-245704142
-# andhttps://github.com/JamesHageman/whistlet-elm/blob/1bc50446987e3bcf92a9710698ae417652d7517f/circle-deps.sh
+# and https://github.com/JamesHageman/whistlet-elm/blob/1bc50446987e3bcf92a9710698ae417652d7517f/circle-deps.sh
 export INSTALL_PATH="$HOME/dependencies"
 
 if [ ! -d $INSTALL_PATH/sysconfcpus/bin ]; then
