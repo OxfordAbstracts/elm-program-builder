485f926649c122004fe60bc6f2d4bd8494fef319
naazy <marziyeh.naaz.ahmed@gmail.com>
2017-01-30 10:27:38 +0000

    addressing @roryc89's comments

--- src/MainUpdate.elm
+++ src/MainUpdate.elm
@@ -39,169 +39,156 @@ toInt model string =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        NewTrack ->
-            ( model, Cmd.none )
-
-        NewColumn ->
-            ( model, Cmd.none )
-
-        ToggleNewSessionUi ->
-            ( { model
-                | showNewSessionUi = not model.showNewSessionUi
-                , showNewColumnUi = False
-                , showNewTrackUi = False
-                , idOfSessionBeingEdited = Nothing
-              }
-            , Cmd.none
-            )
-
-        ToggleNewTrackUi ->
-            ( { model
-                | showNewTrackUi = not model.showNewTrackUi
-                , showNewColumnUi = False
-                , showNewSessionUi = False
-                , idOfSessionBeingEdited = Nothing
-              }
-            , Cmd.none
-            )
-
-        ToggleNewColumnUi ->
-            ( { model
-                | showNewColumnUi = not model.showNewColumnUi
-                , showNewSessionUi = False
-                , showNewTrackUi = False
-                , idOfSessionBeingEdited = Nothing
-              }
-            , Cmd.none
-            )
-
-        CreateNewColumn ->
+    let
+        appendNewElementToList list newElement =
             let
-                highestColumnId =
-                    model.columns
+                highestId =
+                    list
                         |> List.map .id
                         |> List.maximum
                         |> Maybe.withDefault 0
 
-                newColumn =
-                    model.newColumn
-
-                newColumnWithId =
-                    { newColumn | id = highestColumnId + 1 }
+                newElementWithId =
+                    { newElement | id = highestId + 1 }
             in
+                list ++ [ newElementWithId ]
+    in
+        case msg of
+            NewTrack ->
+                ( model, Cmd.none )
+
+            NewColumn ->
+                ( model, Cmd.none )
+
+            ToggleNewSessionUi ->
                 ( { model
-                    | columns = model.columns ++ [ newColumnWithId ]
-                    , newColumn = blankColumn 1
+                    | showNewSessionUi = not model.showNewSessionUi
+                    , showNewColumnUi = False
+                    , showNewTrackUi = False
+                    , idOfSessionBeingEdited = Nothing
                   }
                 , Cmd.none
                 )
 
-        CreateNewSession ->
-            let
-                newSessionId =
-                    model.sessions
-                        |> List.map .id
-                        |> List.maximum
-                        |> Maybe.withDefault 1
-
-                newSession =
-                    model.newSession
-
-                newSessionWithId =
-                    { newSession | id = newSessionId }
-            in
+            ToggleNewTrackUi ->
                 ( { model
-                    | sessions = model.sessions ++ [ newSessionWithId ]
-                    , newSession = blankSession 1
-                  }
-                , Cmd.none
-                )
-
-        CreateNewTrack ->
-            let
-                highestTrackId =
-                    model.tracks
-                        |> List.map .id
-                        |> List.maximum
-                        |> Maybe.withDefault 0
-
-                newTrack =
-                    model.newTrack
-
-                newTrackWithId =
-                    { newTrack | id = highestTrackId + 1 }
-            in
-                ( { model
-                    | tracks = model.tracks ++ [ newTrackWithId ]
-                    , newTrack = blankTrack 1
-                  }
-                , Cmd.none
-                )
-
-        UpdateNewColumnName newName ->
-            ( (updateNewColumn model (\ns -> { ns | name = newName })), Cmd.none )
-
-        UpdateNewSessionName newName ->
-            ( (updateNewSession model (\ns -> { ns | name = newName })), Cmd.none )
-
-        UpdateNewTrackName newName ->
-            ( (updateNewTrack model (\ns -> { ns | name = newName })), Cmd.none )
-
-        UpdateNewSessionDescription newDescription ->
-            ( (updateNewSession model (\ns -> { ns | description = newDescription })), Cmd.none )
-
-        UpdateNewSessionColumn newColumnId ->
-            ( (updateNewSession model (\ns -> { ns | columnId = (toInt model newColumnId) })), Cmd.none )
-
-        UpdateNewSessionChair newChair ->
-            ( (updateNewSession model (\ns -> { ns | chair = newChair })), Cmd.none )
-
-        UpdateNewSessionLocation newLocation ->
-            ( (updateNewSession model (\ns -> { ns | location = newLocation })), Cmd.none )
-
-        UpdateNewSessionTrack newTrackId ->
-            ( (updateNewSession model (\ns -> { ns | trackId = (toInt model newTrackId) })), Cmd.none )
-
-        UpdateNewSessionDate newDate ->
-            ( (updateNewSession model (\ns -> { ns | date = (DateUtils.valueStringToDateWithoutTime newDate) })), Cmd.none )
-
-        UpdateNewSessionStartHour new ->
-            ( updateNewSessionStartTime model (\st -> { st | hour = clamp 0 23 (toInt model new) }), Cmd.none )
-
-        UpdateNewSessionStartMinute new ->
-            ( updateNewSessionStartTime model (\st -> { st | minute = clamp 0 59 (toInt model new) }), Cmd.none )
-
-        UpdateNewSessionEndHour new ->
-            ( updateNewSessionEndTime model (\et -> { et | hour = clamp 0 23 (toInt model new) }), Cmd.none )
-
-        UpdateNewSessionEndMinute new ->
-            ( updateNewSessionEndTime model (\et -> { et | minute = clamp 0 59 (toInt model new) }), Cmd.none )
-
-        DeleteSession sessionId ->
-            ( { model | sessions = List.filter (\s -> s.id /= sessionId) model.sessions }, Cmd.none )
-
-        SelectSessionToEdit sessionId ->
-            let
-                isAlreadySelected =
-                    model.idOfSessionBeingEdited
-                        |> Maybe.withDefault -1
-                        |> (\id -> id == sessionId)
-
-                session =
-                    model.sessions
-                        |> List.filter (\s -> s.id == sessionId)
-                        |> List.head
-                        |> Maybe.withDefault (blankSession -1)
-            in
-                ( { model
-                    | idOfSessionBeingEdited =
-                        if isAlreadySelected then
-                            Nothing
-                        else
-                            Just sessionId
+                    | showNewTrackUi = not model.showNewTrackUi
+                    , showNewColumnUi = False
                     , showNewSessionUi = False
-                    , newSession = session
+                    , idOfSessionBeingEdited = Nothing
                   }
                 , Cmd.none
                 )
+
+            ToggleNewColumnUi ->
+                ( { model
+                    | showNewColumnUi = not model.showNewColumnUi
+                    , showNewSessionUi = False
+                    , showNewTrackUi = False
+                    , idOfSessionBeingEdited = Nothing
+                  }
+                , Cmd.none
+                )
+
+            CreateNewColumn ->
+                let
+                    listWithNewId =
+                        appendNewElementToList model.columns model.newColumn
+                in
+                    ( { model
+                        | columns = listWithNewId
+                        , newColumn = blankColumn 1
+                      }
+                    , Cmd.none
+                    )
+
+            CreateNewSession ->
+                let
+                    listWithNewId =
+                        appendNewElementToList model.sessions model.newSession
+                in
+                    ( { model
+                        | sessions = listWithNewId
+                        , newSession = blankSession 1
+                      }
+                    , Cmd.none
+                    )
+
+            CreateNewTrack ->
+                let
+                    listWithNewId =
+                        appendNewElementToList model.tracks model.newTrack
+                in
+                    ( { model
+                        | tracks = listWithNewId
+                        , newTrack = blankTrack 1
+                      }
+                    , Cmd.none
+                    )
+
+            UpdateNewColumnName newName ->
+                ( (updateNewColumn model (\ns -> { ns | name = newName })), Cmd.none )
+
+            UpdateNewSessionName newName ->
+                ( (updateNewSession model (\ns -> { ns | name = newName })), Cmd.none )
+
+            UpdateNewTrackName newName ->
+                ( (updateNewTrack model (\ns -> { ns | name = newName })), Cmd.none )
+
+            UpdateNewSessionDescription newDescription ->
+                ( (updateNewSession model (\ns -> { ns | description = newDescription })), Cmd.none )
+
+            UpdateNewSessionColumn newColumnId ->
+                ( (updateNewSession model (\ns -> { ns | columnId = (toInt model newColumnId) })), Cmd.none )
+
+            UpdateNewSessionChair newChair ->
+                ( (updateNewSession model (\ns -> { ns | chair = newChair })), Cmd.none )
+
+            UpdateNewSessionLocation newLocation ->
+                ( (updateNewSession model (\ns -> { ns | location = newLocation })), Cmd.none )
+
+            UpdateNewSessionTrack newTrackId ->
+                ( (updateNewSession model (\ns -> { ns | trackId = (toInt model newTrackId) })), Cmd.none )
+
+            UpdateNewSessionDate newDate ->
+                ( (updateNewSession model (\ns -> { ns | date = (DateUtils.valueStringToDateWithoutTime newDate) })), Cmd.none )
+
+            UpdateNewSessionStartHour new ->
+                ( updateNewSessionStartTime model (\st -> { st | hour = clamp 0 23 (toInt model new) }), Cmd.none )
+
+            UpdateNewSessionStartMinute new ->
+                ( updateNewSessionStartTime model (\st -> { st | minute = clamp 0 59 (toInt model new) }), Cmd.none )
+
+            UpdateNewSessionEndHour new ->
+                ( updateNewSessionEndTime model (\et -> { et | hour = clamp 0 23 (toInt model new) }), Cmd.none )
+
+            UpdateNewSessionEndMinute new ->
+                ( updateNewSessionEndTime model (\et -> { et | minute = clamp 0 59 (toInt model new) }), Cmd.none )
+
+            DeleteSession sessionId ->
+                ( { model | sessions = List.filter (\s -> s.id /= sessionId) model.sessions }, Cmd.none )
+
+            SelectSessionToEdit sessionId ->
+                let
+                    isAlreadySelected =
+                        model.idOfSessionBeingEdited
+                            |> Maybe.withDefault -1
+                            |> (\id -> id == sessionId)
+
+                    session =
+                        model.sessions
+                            |> List.filter (\s -> s.id == sessionId)
+                            |> List.head
+                            |> Maybe.withDefault (blankSession -1)
+                in
+                    ( { model
+                        | idOfSessionBeingEdited =
+                            if isAlreadySelected then
+                                Nothing
+                            else
+                                Just sessionId
+                        , showNewSessionUi = False
+                        , newSession = session
+                      }
+                    , Cmd.none
+                    )
